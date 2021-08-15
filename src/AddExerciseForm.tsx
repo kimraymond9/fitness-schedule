ad83e680083bef9a07651bf7eea90d22fa2e928c
Raymond <kimraymond9@gmail.com>
2021-08-15 22:49:06 +1200

    Create an add exercise form component that stores exercises in local storage.

---
+++ src/AddExerciseForm.tsx
@@ -0,0 +1,66 @@
+import { Button, FormControl, FormGroup, TextField } from '@material-ui/core';
+import { ChangeEvent, MouseEvent, useState, FC } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
+
+const AddExerciseForm:  FC = () => {
+
+  const [formValues, setFormValues] = useState({description: "", sets: "", reps: ""});
+  
+  const [localStorageExercise, setLocalStorageExercise] = useLocalStorage('monday', []);
+
+  const { description, sets, reps } = formValues;
+
+  const handleFormSubmit = () => {
+    const exercise = {
+      description: description,
+      sets: sets,
+      reps: reps
+    }
+    setLocalStorageExercise(localStorageExercise.concat(exercise));
+  };
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  return (
+    <FormControl fullWidth>
+      <FormGroup>
+        <TextField
+          margin="dense"
+          id="standard-basic" 
+          label="Description" 
+          name="description"
+          value={formValues.description}
+          onChange={onInputChange}
+        />
+        <TextField
+          margin="dense"
+          id="standard-basic" 
+          label="Sets" 
+          name="sets"
+          value={formValues.sets}
+          onChange={onInputChange}
+        />
+        <TextField
+          margin="dense"
+          id="standard-basic" 
+          label="Reps" 
+          name="reps"
+          value={formValues.reps}
+          onChange={onInputChange}
+        />
+        <Button
+          variant="contained"
+          onClick={handleFormSubmit}
+          type="submit"
+          color="primary">
+              ADD
+        </Button>
+      </FormGroup>
+    </FormControl>
+  );
+}
+
+export default AddExerciseForm;
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import AddExerciseForm from './AddExerciseForm';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+const App = () => {
 
-export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v5-beta example with TypeScript
+          Fitness Schedule
         </Typography>
-        <ProTip />
-        <Copyright />
+        <AddExerciseForm/>
       </Box>
     </Container>
   );
 }
+
+export default App;
