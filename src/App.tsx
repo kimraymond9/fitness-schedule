114ca6c5bf187fe027bb56d7a67f516081c17f95
Raymond <kimraymond9@gmail.com>
2021-08-16 22:56:36 +1200

    Added a weight input and improved the UX of the add exercise form component. Respond to PR comments about refactorings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import AddExerciseForm from './AddExerciseForm';
 
 const App = () => (
-  <Container maxWidth="sm">
-    <Box sx={{ my: 4 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Fitness Schedule
-      </Typography>
-      <AddExerciseForm />
-    </Box>
+  <Container>
+    <Typography variant="h2" gutterBottom>
+      Fitness Schedule
+    </Typography>
+    <AddExerciseForm />
   </Container>
 );
 
